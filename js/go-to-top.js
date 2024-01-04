4c900c1eb6cb1d597723802dc2e2db1fb6c467cb
Amy Nguyen (s3783694) <nguyena.amy@gmail.com>
2024-01-04 19:29:45 +1100

    Update styling and content

--- js/go-to-top.js
+++ js/go-to-top.js
@@ -1,16 +1,31 @@
 // Scroll to top function
-$(window).scroll(function() {
-	var height = $(window).scrollTop();
-	if (height > 100) {
-			$('#go-to-top').fadeIn();
-	} else {
-			$('#go-to-top').fadeOut();
-	}
-});
-$(document).ready(function() {
-	$("#go-to-top").click(function(event) {
-			event.preventDefault();
-			$("html, body").animate({ scrollTop: 0 }, "slow");
-			return false;
-	});	
-});
+// $(window).scroll(function() {
+// 	var height = $(window).scrollTop();
+// 	if (height > 100) {
+// 			$('#go-to-top').fadeIn();
+// 	} else {
+// 			$('#go-to-top').fadeOut();
+// 	}
+// });
+// $(document).ready(function() {
+// 	$("#go-to-top").click(function(event) {
+// 			event.preventDefault();
+// 			$("html, body").animate({ scrollTop: 0 }, "smooth");
+// 			return false;
+// 	});	
+// });
+
+window.onscroll = () => {
+    toggleTopButton();
+}
+function scrollToTop(){
+window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function toggleTopButton() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('go-to-top').classList.remove('d-none');
+    } else {
+        document.getElementById('go-to-top').classList.add('d-none');
+    }
+}
